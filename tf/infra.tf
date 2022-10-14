351a23c995b92554f4f6567edc7a5050b104e4d4
Julien PERIGNON <perignon.julien@gmail.com>
2022-10-14 12:02:18 +1100

    Update infra.tf

--- tf/infra.tf
+++ tf/infra.tf
@@ -5,7 +5,7 @@ variable "secret_key" {}
 provider "aws" {
   region  = "${var.region}"
   access_key = "${var.access_key}"
-  secret_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 variable "web-open-ports" {
